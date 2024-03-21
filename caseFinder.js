12ad700cce0a0bbfab1be6076cb534b786370bf5
Syed Anas Bukhari <anas14529@gmail.com>
2024-03-21 23:57:25 +0500

    Link Fixed.

--- caseFinder.js
+++ caseFinder.js
@@ -1,9 +1,12 @@
 // Event listener to handle messages posted from the parent window
 window.addEventListener('message', function (event) {
     // Check if the message is from an allowed origin
-    if (event.data.bodyHeight) { // Replace 'http://example.com' with the actual origin of the parent window
+    if (event.data.bodyHeight) {
         let height = event.data.bodyHeight;
         console.log('Received message from parent window:', height);
         document.getElementById('caseFinderIframe').style.height = height + 'px';
+    }else if (event.data.URL) {
+        let url = event.data.URL;
+        this.window.location = url;
     }
 });
